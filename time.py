954c095ce0d3e6f118488dc4a8c4fa84c76f961a
aritra6524 <76067809+aritra6524@users.noreply.github.com>
2021-05-19 20:42:16 +0530

    Add files via upload

---
+++ time.py
@@ -0,0 +1,20 @@
+from tkinter import Tk
+from tkinter import Label
+import time
+import sys
+
+master = Tk()
+master.title("Digital Clock")
+
+def get_time():
+    timeVar = time.strftime("%I:%M:%S %p")
+    clock.config(text=timeVar)
+    clock.after(200,get_time)
+
+Label(master,font=("Arial",30),text="Digital Clock",fg="black").pack()
+clock = Label(master, font=("Arial",100),fg="white",bg="black")
+clock.pack()
+
+get_time()
+
+master.mainloop()
